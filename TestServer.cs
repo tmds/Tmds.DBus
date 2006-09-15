e8ac3c02d793460c4a9862319c1794cb6f2a6b0f
Alp Toker <alp@atoker.com>
2006-09-15 18:47:44 +0100

    Add server|client mode to server demo

--- TestServer.cs
+++ TestServer.cs
@@ -17,29 +17,73 @@ public class TestServer
 	//TODO: maybe generalise it and integrate it into the core
 	public static void Main (string[] args)
 	{
+		bool isServer;
+
+		if (args.Length == 1 && args[0] == "server")
+			isServer = true;
+		else if (args.Length == 1 && args[0] == "client")
+			isServer = false;
+		else {
+			Console.Error.WriteLine ("Usage: test-server [server|client]");
+			return;
+		}
+
 		string addr = "unix:abstract=/tmp/dbus-ABCDEFGHIJ";
 
-		string path = "/tmp/dbus-ABCDEFGHIJ";
-		bool @abstract = true;
+		Connection conn;
 
-		AbstractUnixEndPoint ep = new AbstractUnixEndPoint (path);
-		Socket server = new Socket (AddressFamily.Unix, SocketType.Stream, 0);
-		server.Bind (ep);
+		DemoObject demo;
 
-		server.Listen (1);
 
-		Console.WriteLine ("Waiting for client on " + addr);
-		Socket client = server.Accept ();
-		Console.WriteLine ("Client accepted");
+		ObjectPath myOpath = new ObjectPath ("/test");
+		string myNameReq = "org.ndesk.test";
 
-		//this might well be wrong, untested and doesn't yet work here onwards
-		Connection conn = new Connection (false);
-		//conn.Open (path, @abstract);
-		conn.sock = client;
-		conn.sock.Blocking = true;
-		conn.ns = new NetworkStream (conn.sock);
 
-		while (true)
-			conn.Iterate ();
+		if (!isServer) {
+			conn = new Connection (false);
+			conn.Open (addr);
+			demo = conn.GetObject<DemoObject> (myNameReq, myOpath);
+			demo.Hello ("hi from test client", 21);
+		} else {
+			string path;
+			bool abstr;
+
+			Address.Parse (addr, out path, out abstr);
+
+			AbstractUnixEndPoint ep = new AbstractUnixEndPoint (path);
+			Socket server = new Socket (AddressFamily.Unix, SocketType.Stream, 0);
+
+			server.Bind (ep);
+			server.Listen (1);
+
+			Console.WriteLine ("Waiting for client on " + addr);
+			Socket client = server.Accept ();
+			Console.WriteLine ("Client accepted");
+
+			//this might well be wrong, untested and doesn't yet work here onwards
+			conn = new Connection (false);
+			//conn.Open (path, @abstract);
+			conn.sock = client;
+			conn.sock.Blocking = true;
+			conn.ns = new NetworkStream (conn.sock);
+
+			Connection.tmpConn = conn;
+
+			demo = new DemoObject ();
+			conn.Marshal (demo, "org.ndesk.test");
+
+			//TODO: handle lost connections etc.
+			while (true)
+				conn.Iterate ();
+		}
+	}
+}
+
+[Interface ("org.ndesk.test")]
+public class DemoObject : MarshalByRefObject
+{
+	public void Hello (string arg0, int arg1)
+	{
+		Console.WriteLine ("Got a Hello(" + arg0 + ", " + arg1 +")");
 	}
 }

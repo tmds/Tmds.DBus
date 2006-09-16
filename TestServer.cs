27e04506925b166339a44be1356dc6aa1be4f7de
Alp Toker <alp@atoker.com>
2006-09-16 05:01:16 +0100

    Add threading to test server

--- TestServer.cs
+++ TestServer.cs
@@ -11,6 +11,8 @@ using System.Net;
 using System.Net.Sockets;
 using Mono.Unix;
 
+using System.Threading;
+
 public class TestServer
 {
 	//TODO: complete this test daemon/server example, and a client
@@ -32,19 +34,20 @@ public class TestServer
 
 		Connection conn;
 
-		DemoObject demo;
-
-
 		ObjectPath myOpath = new ObjectPath ("/test");
 		string myNameReq = "org.ndesk.test";
-
 
 		if (!isServer) {
 			conn = new Connection (false);
 			conn.Open (addr);
-			demo = conn.GetObject<DemoObject> (myNameReq, myOpath);
-			float ret = demo.Hello ("hi from test client", 21);
-			Console.WriteLine ("Returned float: " + ret);
+			DemoObject demo = conn.GetObject<DemoObject> (myNameReq, myOpath);
+			//float ret = demo.Hello ("hi from test client", 21);
+			float ret = 200;
+			while (ret > 5) {
+				ret = demo.Hello ("hi from test client", (int)ret);
+				Console.WriteLine ("Returned float: " + ret);
+				System.Threading.Thread.Sleep (1000);
+			}
 		} else {
 			string path;
 			bool abstr;
@@ -55,27 +58,60 @@ public class TestServer
 			Socket server = new Socket (AddressFamily.Unix, SocketType.Stream, 0);
 
 			server.Bind (ep);
-			server.Listen (1);
+			//server.Listen (1);
+			server.Listen (5);
 
-			Console.WriteLine ("Waiting for client on " + addr);
-			Socket client = server.Accept ();
-			Console.WriteLine ("Client accepted");
+			while (true) {
+				Console.WriteLine ("Waiting for client on " + addr);
+				Socket client = server.Accept ();
+				Console.WriteLine ("Client accepted");
 
-			//this might well be wrong, untested and doesn't yet work here onwards
-			conn = new Connection (false);
-			//conn.Open (path, @abstract);
-			conn.sock = client;
-			conn.sock.Blocking = true;
-			conn.ns = new NetworkStream (conn.sock);
+				//this might well be wrong, untested and doesn't yet work here onwards
+				conn = new Connection (false);
+				//conn.Open (path, @abstract);
+				conn.sock = client;
+				conn.sock.Blocking = true;
+				conn.ns = new NetworkStream (conn.sock);
 
-			Connection.tmpConn = conn;
+				//ConnectionHandler.Handle (conn);
 
-			demo = new DemoObject ();
-			conn.Marshal (demo, "org.ndesk.test");
+				//in reality a thread per connection is of course too expensive
+				ConnectionHandler hnd = new ConnectionHandler (conn);
+				new Thread (new ThreadStart (hnd.Handle)).Start ();
 
-			//TODO: handle lost connections etc.
-			while (true)
-				conn.Iterate ();
+				Console.WriteLine ();
+			}
+		}
+	}
+}
+
+public class ConnectionHandler
+{
+	protected Connection conn;
+
+	public ConnectionHandler (Connection conn)
+	{
+		this.conn = conn;
+	}
+
+	public void Handle ()
+	{
+		ConnectionHandler.Handle (conn);
+	}
+
+	public static void Handle (Connection conn)
+	{
+		//Connection.tmpConn = conn;
+
+		DemoObject demo = new DemoObject ();
+		conn.Marshal (demo, "org.ndesk.test");
+
+		//TODO: handle lost connections etc. properly instead of stupido try/catch
+		try {
+		while (true)
+			conn.Iterate ();
+		} catch (Exception e) {
+			//Console.Error.WriteLine (e);
 		}
 	}
 }

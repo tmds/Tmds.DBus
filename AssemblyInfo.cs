aebde6a97ccb81d59bb720086f3e19222c8b38a4
Alp Toker <alp@atoker.com>
2006-09-21 05:15:52 +0100

    Add AssemblyInfo

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyTitle ("NDesk.DBus")]
+[assembly: AssemblyDescription ("DBus protocol library and CLR binding")]
+[assembly: AssemblyCopyright ("Copyright (C) Alp Toker")]
+[assembly: AssemblyCompany ("NDesk")]

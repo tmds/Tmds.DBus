86d111570f9be60471715826fa3c94b5a71471d2
Alp Toker <alp@atoker.com>
2006-09-23 09:04:07 +0100

    More descriptive assembly description

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyVersion("0.0.0.*")]
 [assembly: AssemblyTitle ("NDesk.DBus")]
-[assembly: AssemblyDescription ("D-Bus protocol library and CLR binding")]
+[assembly: AssemblyDescription ("D-Bus IPC protocol library and CLR binding")]
 [assembly: AssemblyCopyright ("Copyright (C) Alp Toker")]
 [assembly: AssemblyCompany ("NDesk")]

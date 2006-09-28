9405ad47ddba705ef5bce8dc541e0a9ec29e3f38
Alp Toker <alp@atoker.com>
2006-09-28 22:05:20 +0100

    Add copyright header

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,3 +1,7 @@
+// Copyright 2006 Alp Toker <alp@atoker.com>
+// This software is made available under the MIT License
+// See COPYING for details
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 

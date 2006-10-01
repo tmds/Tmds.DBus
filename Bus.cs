88b760743f60f7050cdbda496b50df5819ec04b0
Alp Toker <alp@atoker.com>
2006-10-01 07:40:52 +0100

    Use alternative (non-DProxy) implementation of Bus for now
    Should work the same way and perform better until IL gen is done.

--- Bus.cs
+++ Bus.cs
@@ -62,15 +62,17 @@ namespace NDesk.DBus
 			return bus;
 		}
 
-		//protected org.freedesktop.DBus.Bus bus_proxy;
-		protected IBus bus;
+		//protected IBus bus;
+		private BusObject bus;
 
 		static readonly string DBusName = "org.freedesktop.DBus";
 		static readonly ObjectPath DBusPath = new ObjectPath ("/org/freedesktop/DBus");
 
 		public Bus (string address) : base (address)
 		{
-			bus = GetObject<IBus> (DBusName, DBusPath);
+			//bus = GetObject<IBus> (DBusName, DBusPath);
+			bus = new BusObject (this, DBusName, DBusPath);
+
 			/*
 					bus.NameAcquired += delegate (string acquired_name) {
 			Console.WriteLine ("NameAcquired: " + acquired_name);
@@ -81,6 +83,7 @@ namespace NDesk.DBus
 			Iterate ();
 		}
 
+		/*
 		protected void Register ()
 		{
 			unique_name = bus.Hello ();
@@ -138,6 +141,64 @@ namespace NDesk.DBus
 		{
 			bus.RemoveMatch (rule);
 			//Iterate ();
+		}
+		*/
+
+		protected void Register ()
+		{
+			unique_name = (string)bus.InvokeMethod (typeof (IBus).GetMethod ("Hello"));
+		}
+
+		public override string UniqueName
+		{
+			get {
+				return unique_name;
+			}
+		}
+
+		public ulong GetUnixUser (string name)
+		{
+			return (ulong)bus.InvokeMethod (typeof (IBus).GetMethod ("GetConnectionUnixUser"), name);
+		}
+
+		public NameReply RequestName (string name)
+		{
+			return RequestName (name, NameFlag.None);
+		}
+
+		public NameReply RequestName (string name, NameFlag flags)
+		{
+			return (NameReply)bus.InvokeMethod (typeof (IBus).GetMethod ("RequestName"), name, flags);
+		}
+
+		public ReleaseNameReply ReleaseName (string name)
+		{
+			return (ReleaseNameReply)bus.InvokeMethod (typeof (IBus).GetMethod ("ReleaseName"), name);
+		}
+
+		public bool NameHasOwner (string name)
+		{
+			return (bool)bus.InvokeMethod (typeof (IBus).GetMethod ("NameHasOwner"), name);
+		}
+
+		public StartReply StartServiceByName (string name)
+		{
+			return StartServiceByName (name, 0);
+		}
+
+		public StartReply StartServiceByName (string name, uint flags)
+		{
+			return (StartReply)bus.InvokeMethod (typeof (IBus).GetMethod ("StartServiceByName"), name, flags);
+		}
+
+		public override void AddMatch (string rule)
+		{
+			bus.InvokeMethod (typeof (IBus).GetMethod ("AddMatch"), rule);
+		}
+
+		public override void RemoveMatch (string rule)
+		{
+			bus.InvokeMethod (typeof (IBus).GetMethod ("RemoveMatch"), rule);
 		}
 	}
 }

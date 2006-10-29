bc33c734a0cb393a84f2de0155007c40f68119e1
Alp Toker <alp@atoker.com>
2006-10-29 13:31:09 +0000

    Enable undocumented methods but mark them Obsolete

--- DBus.cs
+++ DBus.cs
@@ -105,12 +105,14 @@ namespace org.freedesktop.DBus
 		void AddMatch (string rule);
 		void RemoveMatch (string rule);
 
-#if UNDOCUMENTED_IN_SPEC
 		//undocumented in spec
+		[Obsolete ("Undocumented in spec")]
 		string[] ListQueuedOwners (string name);
+		[Obsolete ("Undocumented in spec")]
 		uint GetConnectionUnixProcessID (string connection_name);
+		[Obsolete ("Undocumented in spec")]
 		byte[] GetConnectionSELinuxSecurityContext (string connection_name);
+		[Obsolete ("Undocumented in spec")]
 		void ReloadConfig ();
-#endif
 	}
 }

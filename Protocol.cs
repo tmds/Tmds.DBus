c077c435606679737f66d19ade8d8ff1286b409d
Alp Toker <alp@atoker.com>
2006-12-05 17:07:52 +0000

    Do not accept messages with version higher than 1

--- Protocol.cs
+++ Protocol.cs
@@ -174,11 +174,7 @@ namespace NDesk.DBus
 		//protocol versions that we support
 		public const byte MinVersion = 0;
 		public const byte Version = 1;
-#if PROTO_TYPE_SINGLE
-		public const byte MaxVersion = 2;
-#else
-		public const byte MaxVersion = 1;
-#endif
+		public const byte MaxVersion = Version;
 
 		public static int PadNeeded (int pos, int alignment)
 		{

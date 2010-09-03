180d50608e7ec72027a91e7b3453cc0cf8c4f7b1
Jérémie Laval <jeremie.laval@gmail.com>
2010-09-03 09:29:32 +0100

    Remove dead code in TypeImplementer

--- TypeImplementer.cs
+++ TypeImplementer.cs
@@ -15,33 +15,12 @@ namespace NDesk.DBus
 		AssemblyBuilder asmB;
 		ModuleBuilder modB;
 		static object getImplLock = new Object ();
-		//List<Assembly> cacheAs = new List<Assembly> ();
 
 		public TypeImplementer (string name, bool canSave)
 		{
-			//asmB = AppDomain.CurrentDomain.DefineDynamicAssembly (new AssemblyName ("NDesk.DBus.Proxies"), AssemblyBuilderAccess.Run);
 			asmB = AppDomain.CurrentDomain.DefineDynamicAssembly (new AssemblyName ("NDesk.DBus.Proxies"), canSave ? AssemblyBuilderAccess.RunAndSave : AssemblyBuilderAccess.Run);
 			modB = asmB.DefineDynamicModule ("NDesk.DBus.Proxies");
-			//modB = asmB.DefineDynamicModule ("NDesk.DBus.Proxies.dll", "NDesk.DBus.Proxies.dll");
-			//Load ("NDesk.DBus.Proxies");
 		}
-
-		/*
-		public void Load (string fname)
-		{
-			try {
-				cacheAs.Add (AppDomain.CurrentDomain.Load (fname));
-			} catch {
-			}
-		}
-
-		public void Save ()
-		{
-			string fname = "NDesk.DBus.Proxies.dll";
-			System.IO.File.Delete (fname);
-			asmB.Save (fname);
-		}
-		*/
 
 		Dictionary<Type,Type> map = new Dictionary<Type,Type> ();
 
@@ -55,17 +34,6 @@ namespace NDesk.DBus
 
 				string proxyName = declType.FullName + "Proxy";
 
-				/*
-				foreach (Assembly cacheA in cacheAs) {
-					Type rt = cacheA.GetType (proxyName, false);
-					if (rt != null) {
-						Console.WriteLine ("HIT " + rt);
-						map[declType] = rt;
-						return rt;
-					}
-				}
-				*/
-
 				Type parentType;
 
 				if (declType.IsInterface)
@@ -74,49 +42,6 @@ namespace NDesk.DBus
 					parentType = declType;
 
 				TypeBuilder typeB = modB.DefineType (proxyName, TypeAttributes.Class | TypeAttributes.Public, parentType);
-/*
-				if (false && !declType.IsInterface) {
-					foreach (MethodInfo mi in declType.GetMethods (BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)) {
-						MethodBody body = mi.GetMethodBody ();
-						Console.WriteLine(mi + ":");
-						foreach (LocalVariableInfo lvar in body.LocalVariables) {
-							Console.WriteLine(lvar.LocalIndex + ": " + lvar.LocalType);
-						}
-						Console.WriteLine();
-
-						//DynamicMethod method_builder = new DynamicMethod ("Write" + t.Name, typeof (void), new Type[] {typeof (MessageWriter), t}, typeof (MessageWriter));
-						//DynamicMethod dm = new DynamicMethod ("RepProx", typeof(void), new Type[] { typeof(object), typeof(int), typeof(string) }, typeof(object));
-						//DynamicILInfo dil = dm.GetDynamicILInfo ();
-						//dil.SetCode(body.GetILAsByteArray (), body.MaxStackSize);
-
-						//MethodBuilder mb = typeB.DefineMethod ("RepProx", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { typeof(object), typeof(int), typeof(string) });
-						//MethodBuilder mb = typeB.DefineMethod ("RepProx", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { declType, typeof(int), typeof(string) });
-						//
-
-					//MethodAttributes attrs = mi.Attributes;
-				 	//^ MethodAttributes.Abstract;
-					//attrs ^= MethodAttributes.NewSlot;
-					//attrs |= MethodAttributes.Final;
-
-					//MethodAttributes.Public
-					MethodAttributes attrs = MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.HideBySig;
-					Console.WriteLine("cwl attrs: " + typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }).Attributes);
-						//DynamicILInfo dil = mi.GetDynamicILInfo ();
-
-						ParameterInfo[] parms = mi.GetParameters();
-						//MethodBuilder mb = typeB.DefineMethod ("RepProx", attrs, typeof(void), new Type[] { typeof(int), typeof(string) });
-						//MethodBuilder mb = typeB.DefineMethod ("RepProx", attrs, typeof(void), new Type[] { declType, typeof(int), typeof(string) });
-						MethodBuilder mb = typeB.DefineMethod ("RepProx", attrs, typeof(void), new Type[] { typeof(object), typeof(int), typeof(string) });
-
-						mb.DefineParameter (0, ParameterAttributes.None, "self");
-						for (int i = 0; i < parms.Length ; i++) {
-							Console.WriteLine("i={0} pos={1} {2} {3}", i, parms[i].Position, parms[i].Attributes, parms[i]);
-							mb.DefineParameter (parms[i].Position + 1, parms[i].Attributes, parms[i].Name);
-						}
-
-					}
-				}
-*/
 
 				if (declType.IsInterface)
 					Implement (typeB, declType);
@@ -154,7 +79,6 @@ namespace NDesk.DBus
 				for (int i = 0; i < parms.Length ; i++)
 					method_builder.DefineParameter (i + 1, parms[i].Attributes, parms[i].Name);
 
-				//Console.WriteLine ("retType: " + declMethod.ReturnType);
 				ILGenerator ilg = method_builder.GetILGenerator ();
 				GenHookupMethod (ilg, declMethod, sendMethodCallMethod, Mapper.GetInterfaceName (iface), declMethod.Name);
 
@@ -224,11 +148,9 @@ namespace NDesk.DBus
 			return hookupMethod;
 		}
 
-		//static MethodInfo getMethodFromHandleMethod = typeof (MethodBase).GetMethod ("GetMethodFromHandle", new Type[] {typeof (RuntimeMethodHandle)});
 		static MethodInfo getTypeFromHandleMethod = typeof (Type).GetMethod ("GetTypeFromHandle", new Type[] {typeof (RuntimeTypeHandle)});
 		static ConstructorInfo argumentNullExceptionConstructor = typeof (ArgumentNullException).GetConstructor (new Type[] {typeof (string)});
 		static ConstructorInfo messageWriterConstructor = typeof (MessageWriter).GetConstructor (Type.EmptyTypes);
-		//static MethodInfo messageWriterWriteMethod = typeof (MessageWriter).GetMethod ("WriteComplex", new Type[] {typeof (object), typeof (Type)});
 		static MethodInfo messageWriterWritePad = typeof (MessageWriter).GetMethod ("WritePad", new Type[] {typeof (int)});
 		static MethodInfo messageReaderReadPad = typeof (MessageReader).GetMethod ("ReadPad", new Type[] {typeof (int)});
 
@@ -240,18 +162,6 @@ namespace NDesk.DBus
 
 			if (writeMethods.TryGetValue (t, out meth))
 				return meth;
-
-			/*
-			Type tUnder = t;
-			if (t.IsEnum)
-				tUnder = Enum.GetUnderlyingType (t);
-
-			meth = typeof (MessageWriter).GetMethod ("Write", BindingFlags.ExactBinding | BindingFlags.Instance | BindingFlags.Public, null, new Type[] {tUnder}, null);
-			if (meth != null) {
-				writeMethods[t] = meth;
-				return meth;
-			}
-			*/
 
 			DynamicMethod method_builder = new DynamicMethod ("Write" + t.Name, typeof (void), new Type[] {typeof (MessageWriter), t}, typeof (MessageWriter), true);
 
@@ -293,7 +203,6 @@ namespace NDesk.DBus
 		public static void GenWriter (ILGenerator ilg, Type t)
 		{
 			Type tUnder = t;
-			//bool imprecise = false;
 
 			if (t.IsEnum) {
 				tUnder = Enum.GetUnderlyingType (t);
@@ -307,9 +216,6 @@ namespace NDesk.DBus
 			//ExactBinding InvokeMethod
 
 			if (exactWriteMethod != null) {
-				//if (imprecise)
-				//	ilg.Emit (OpCodes.Castclass, tUnder);
-
 				ilg.Emit (exactWriteMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, exactWriteMethod);
 			} else if (t.IsArray) {
 				MethodInfo mi = typeof (MessageWriter).GetMethod ("WriteArray");
@@ -320,17 +226,7 @@ namespace NDesk.DBus
 				MethodInfo mi = typeof (MessageWriter).GetMethod ("WriteFromDict");
 				exactWriteMethod = mi.MakeGenericMethod (genArgs);
 				ilg.Emit (exactWriteMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, exactWriteMethod);
-			} /*else if (false) {
-				//..boxed if necessary
-				if (t.IsValueType)
-					ilg.Emit (OpCodes.Box, t);
-
-				//the Type parameter
-				ilg.Emit (OpCodes.Ldtoken, t);
-				ilg.Emit (OpCodes.Call, getTypeFromHandleMethod);
-
-				ilg.Emit (messageWriterWriteMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, messageWriterWriteMethod);
-			}*/ else {
+			} else {
 				GenStructWriter (ilg, t);
 			}
 		}
@@ -340,26 +236,6 @@ namespace NDesk.DBus
 			// FIXME: Field order!
 			return type.GetFields (BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		}
-
-		/*
-		public static void GetBlittableSegments (IEnumerable<FieldInfo> fields)
-		{
-			int totalSize = 0;
-			//List<FieldInfo>
-			//foreach (FieldInfo fi in GetMarshalFields (type))
-			foreach (FieldInfo fi in fields) {
-				Type t = fi.FieldType;
-				Signature sig = Signature.GetSig (t);
-				int fixedSize = totalSize;
-				if (sig.GetFixedSize (ref fixedSize)) {
-					totalSize = fixedSize;
-					continue;
-				}
-
-				//ilg.Emit (OpCodes.Ldflda, fiStart);
-			}
-		}
-		*/
 
 		//takes a writer and a reference to an object off the stack
 		public static void GenStructWriter (ILGenerator ilg, Type type)
@@ -414,25 +290,11 @@ namespace NDesk.DBus
 		//takes a reader and a reference to an object off the stack
 		public static void GenStructReader (ILGenerator ilg, Type type)
 		{
-			/*
-			int fixedSize = 0;
-			if (Signature.GetSig (type).GetFixedSize (ref fixedSize)) {
-				Console.Error.WriteLine ("Type " + type + " is blittable.");
-			}
-			*/
-
 			// FIXME: Newobj fails if type has no default ctor!
 
-			//Console.WriteLine ("GenStructReader " + type);
 			LocalBuilder val = ilg.DeclareLocal (type);
 			ConstructorInfo ctor = type.GetConstructor (Type.EmptyTypes);
-			//ConstructorInfo ctor = type.TypeInitializer;
-			//Console.WriteLine ("ctor: " + ctor);
-			//if (ctor != null)
 			ilg.Emit (OpCodes.Newobj, ctor);
-			//ilg.Emit (OpCodes.Ldloca, type);
-			//ilg.Emit (OpCodes.Initobj, type);
-			//if (!type.IsValueType)
 			ilg.Emit (OpCodes.Stloc, val);
 
 			LocalBuilder reader = ilg.DeclareLocal (typeof (MessageReader));
@@ -475,10 +337,6 @@ namespace NDesk.DBus
 			ilg.Emit (OpCodes.Call, getBusObject);
 
 			//MethodInfo
-			/*
-			ilg.Emit (OpCodes.Ldtoken, declMethod);
-			ilg.Emit (OpCodes.Call, getMethodFromHandleMethod);
-			*/
 
 			//interface
 			ilg.Emit (OpCodes.Ldstr, @interface);
@@ -586,15 +444,6 @@ namespace NDesk.DBus
 			ilg.MarkLabel (noErr);
 
 			if (invokeMethod.ReturnType == typeof (MessageReader)) {
-				/*
-				Label notNull = ilg.DefineLabel ();
-				//if the value is null...
-				ilg.Emit (OpCodes.Ldarg, i);
-				ilg.Emit (OpCodes.Brtrue_S, notNull);
-				//was not null, so all is well
-				ilg.MarkLabel (notNull);
-				*/
-
 				LocalBuilder reader = ilg.DeclareLocal (typeof (MessageReader));
 				ilg.Emit (OpCodes.Stloc, reader);
 
@@ -665,7 +514,7 @@ namespace NDesk.DBus
 					continue;
 				if (mi.GetParameters ().Length != 0)
 					continue;
-				//Console.WriteLine ("Adding reader " + mi);
+
 				readMethods[mi.ReturnType] = mi;
 			}
 		}
@@ -679,26 +528,6 @@ namespace NDesk.DBus
 			if (readMethods.TryGetValue (t, out mi))
 				return mi;
 
-			/*
-			Type tIn = t;
-			if (t.IsValueType)
-			*/
-
-			/*
-			DynamicMethod meth = new DynamicMethod ("Read" + t.Name, t, new Type[] {typeof (MessageReader)}, true);
-
-			ILGenerator ilg = meth.GetILGenerator ();
-			ilg.Emit (OpCodes.Ldarg_0);
-			ilg.Emit (OpCodes.Ldarg_1);
-
-			//GenStructReader (ilg, t);
-			GenReader (ilg, t);
-
-			ilg.Emit (OpCodes.Ret);
-
-			writeMethods[t] = meth;
-			return meth;
-			*/
 			return null;
 		}
 
@@ -718,7 +547,6 @@ namespace NDesk.DBus
 
 		internal static DynamicMethod GenReadMethod (MethodInfo target)
 		{
-			//Type[] parms = new Type[] { typeof (object), typeof (MessageReader), typeof (Message) };
 			Type[] parms = new Type[] { typeof (object), typeof (MessageReader), typeof (Message), typeof (MessageWriter) };
 			DynamicMethod hookupMethod = new DynamicMethod ("Caller", typeof (void), parms, typeof (MessageReader));
 			Gen (hookupMethod, target);
@@ -727,14 +555,10 @@ namespace NDesk.DBus
 
 		static void Gen (DynamicMethod hookupMethod, MethodInfo declMethod)
 		{
-			//Console.Error.WriteLine ("Target: " + declMethod);
 			ILGenerator ilg = hookupMethod.GetILGenerator ();
 
 			ParameterInfo[] parms = declMethod.GetParameters ();
 			Type retType = declMethod.ReturnType;
-
-			//if (retType != typeof (void))
-			//	throw new Exception ("Bad retType " + retType);
 
 			// The target instance
 			ilg.Emit (OpCodes.Ldarg_0);
@@ -742,10 +566,6 @@ namespace NDesk.DBus
 			Dictionary<ParameterInfo,LocalBuilder> locals = new Dictionary<ParameterInfo,LocalBuilder> ();
 
 			foreach (ParameterInfo parm in parms) {
-				/*
-				if (parm.IsOut)
-					continue;
-					*/
 
 				Type parmType = parm.ParameterType;
 
@@ -756,18 +576,10 @@ namespace NDesk.DBus
 					continue;
 				}
 
-				/*
-				MethodInfo exactMethod = GetReadMethod (parmType);
-				// The MessageReader instance
-				ilg.Emit (OpCodes.Ldarg_1);
-				ilg.Emit (exactMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, exactMethod);
-				*/
-
 				ilg.Emit (OpCodes.Ldarg_1);
 				GenReader (ilg, parmType);
 			}
 
-			//ilg.Emit (OpCodes.Ldc_I4, 8);
 			ilg.Emit (declMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, declMethod);
 
 			foreach (ParameterInfo parm in parms) {
@@ -784,37 +596,18 @@ namespace NDesk.DBus
 
 			if (retType != typeof (void)) {
 				// Skip reply message construction if MessageWriter is null
-				/*
-				Label needsReply = ilg.DefineLabel ();
-				ilg.Emit (OpCodes.Ldarg_3); // writer
-				ilg.Emit (OpCodes.Brtrue_S, needsReply);
-				ilg.Emit (OpCodes.Pop);
-				//ilg.Emit (OpCodes.Brfalse_S, endLabel);
-				*/
 
-				//Console.WriteLine ("retType: " + retType);
 				LocalBuilder retLocal = ilg.DeclareLocal (retType);
 				ilg.Emit (OpCodes.Stloc, retLocal);
 
-				/*
-				LocalBuilder writer = ilg.DeclareLocal (typeof (MessageWriter));
-				ilg.Emit (OpCodes.Newobj, messageWriterConstructor);
-				ilg.Emit (OpCodes.Stloc, writer);
-				*/
-
 				ilg.Emit (OpCodes.Ldarg_3); // writer
-				//ilg.Emit (OpCodes.Ldloc, writer);
 				ilg.Emit (OpCodes.Ldloc, retLocal);
 				GenWriter (ilg, retType);
 
-				//ilg.Emit (OpCodes.Ldloc, writer);
-				//ilg.MarkLabel (endLabel);
 			}
 
 			ilg.Emit (OpCodes.Ret);
 		}
-
-		// System.MethodAccessException: Method `ManagedDBusTestExport:<Main>m__0 (string,object,double,MyTuple)' is inaccessible from method `(wrapper dynamic-method) object:Caller (object,NDesk.DBus.MessageReader,NDesk.DBus.Message,NDesk.DBus.MessageWriter)'
 
 		//takes the Reader instance off the stack, puts value of type t on the stack
 		public static void GenReader (ILGenerator ilg, Type t)
@@ -823,12 +616,9 @@ namespace NDesk.DBus
 			// TODO: Generate methods with the correct module/type permissions
 
 			Type tUnder = t;
-			//bool imprecise = false;
 
-			if (t.IsEnum) {
+			if (t.IsEnum)
 				tUnder = Enum.GetUnderlyingType (t);
-				//imprecise = true;
-			}
 
 			MethodInfo exactMethod = GetReadMethod (tUnder);
 			if (exactMethod != null)
@@ -842,20 +632,9 @@ namespace NDesk.DBus
 			else if (t.IsInterface)
 				GenFallbackReader (ilg, tUnder);
 			else if (!tUnder.IsValueType) {
-				//Console.Error.WriteLine ("Gen struct reader for " + t);
-				//ilg.Emit (OpCodes.Newobj, messageWriterConstructor);
-				//if (tUnder.IsValueType)
-				//	throw new Exception ("Can't handle value types yet");
 				GenStructReader (ilg, tUnder);
 			} else
 				GenFallbackReader (ilg, tUnder);
-
-			/*
-				//if (t.IsValueType)
-				//	ilg.Emit (OpCodes.Unbox_Any, t);
-				//else
-					ilg.Emit (OpCodes.Castclass, t);
-					*/
 		}
 
 		public static void GenFallbackReader (ILGenerator ilg, Type t)
@@ -881,7 +660,6 @@ namespace NDesk.DBus
 
 		public static void GenReadArrayFixed (ILGenerator ilg, Type t, int knownElemSize)
 		{
-			//Console.Error.WriteLine ("GenReadArrayFixed " + t);
 			LocalBuilder readerLocal = ilg.DeclareLocal (typeof (MessageReader));
 			ilg.Emit (OpCodes.Stloc, readerLocal);
 
@@ -892,27 +670,12 @@ namespace NDesk.DBus
 			Type tUnder = tElem.IsEnum ? Enum.GetUnderlyingType (tElem) : tElem;
 			int managedElemSize = System.Runtime.InteropServices.Marshal.SizeOf (tUnder);
 
-			/*
-			Console.WriteLine ("managedElemSize: " + managedElemSize);
-			Console.WriteLine ("elemSize: " + knownElemSize);
-			Console.WriteLine ("elemSizePadded: " + knownElemSizePadded);
-			*/
-
 			// Read the array's byte length
 			ilg.Emit (OpCodes.Ldloc, readerLocal);
 			MethodInfo exactMethod = GetReadMethod (typeof (uint));
 			ilg.Emit (exactMethod.IsFinal ? OpCodes.Call : OpCodes.Callvirt, exactMethod);
 			LocalBuilder sizeLocal = ilg.DeclareLocal (typeof (uint));
 			ilg.Emit (OpCodes.Stloc, sizeLocal);
-
-			/*
-			// Take the array's byte length
-			ilg.Emit (OpCodes.Ldloc, sizeLocal);
-			// Divide by the known element size
-			//ilg.Emit (OpCodes.Ldc_I4, knownElemSizePadded);
-			ilg.Emit (OpCodes.Ldc_I4, knownElemSize);
-			ilg.Emit (OpCodes.Div_Un);
-			*/
 
 			// Create a new array of the correct element length
 			ilg.Emit (OpCodes.Ldloc, sizeLocal);
@@ -949,7 +712,6 @@ namespace NDesk.DBus
 			// mustBlit: Blit regardless of endian (ie. byte or structs containing only bytes)
 
 			bool shouldBlit = tElem.IsValueType && knownElemSizePadded == managedElemSize && !sigElem.IsStruct;
-			//bool shouldBlit = tElem.IsValueType && knownElemSizePadded == managedElemSize;
 
 			// bool and char are not reliably blittable, so we don't allow blitting in these cases.
 			// Their exact layout varies between runtimes, platforms and even data types.
@@ -958,8 +720,6 @@ namespace NDesk.DBus
 			bool mustBlit = shouldBlit && knownElemSizePadded == 1;
 
 			if (shouldBlit) {
-				//Console.Error.WriteLine ("Blit read array " + tElem);
-
 				if (!mustBlit) {
 					// Check to see if we can blit the data structures
 					FieldInfo nativeEndianField = typeof (MessageReader).GetField ("IsNativeEndian");
@@ -1048,8 +808,6 @@ namespace NDesk.DBus
 
 		public static void GenReadCollection (ILGenerator ilg, Type type)
 		{
-			//Console.WriteLine ("GenReadCollection " + type);
-			//Console.WriteLine ("Sig: " + Signature.GetSig (type));
 			int fixedSize = 0;
 			if (type.IsArray && Signature.GetSig (type.GetElementType ()).GetFixedSize (ref fixedSize)) {
 				GenReadArrayFixed (ilg, type, fixedSize);
@@ -1059,12 +817,8 @@ namespace NDesk.DBus
 			LocalBuilder readerLocal = ilg.DeclareLocal (typeof (MessageReader));
 			ilg.Emit (OpCodes.Stloc, readerLocal);
 
-			//Type[] genArgs = type.GetGenericArguments ();
 			Type[] genArgs = type.IsArray ? new Type[] { type.GetElementType () } : type.GetGenericArguments ();
-			//Type[] genArgs = new Type[] { type.GetElementType () };
-			//Type tElem = genArgs[0];
 
-			//Type collType = Mapper.GetGenericType (typeof (List<>), genArgs);
 			Type collType = Mapper.GetGenericType (genArgs.Length == 2 ? typeof (Dictionary<,>) : typeof (List<>), genArgs);
 
 			ConstructorInfo ctor = collType.GetConstructor (Type.EmptyTypes);
@@ -1073,7 +827,6 @@ namespace NDesk.DBus
 			LocalBuilder collLocal = ilg.DeclareLocal (collType);
 			ilg.Emit (OpCodes.Stloc, collLocal);
 
-			//MethodInfo addMethod = dictType.GetMethod ("Add", new Type[] { tKey, tValue });
 			MethodInfo addMethod = collType.GetMethod ("Add", genArgs);
 
 
@@ -1085,7 +838,6 @@ namespace NDesk.DBus
 			{
 				// Align to 8 for structs
 				ilg.Emit (OpCodes.Ldloc, readerLocal);
-				//ilg.Emit (OpCodes.Ldc_I4, 8);
 				// TODO: This padding logic is sketchy
 				ilg.Emit (OpCodes.Ldc_I4, genArgs.Length > 1 ? 8 : Signature.GetSig (genArgs[0]).Alignment);
 				ilg.Emit (OpCodes.Call, messageReaderReadPad);
@@ -1132,7 +884,6 @@ namespace NDesk.DBus
 
 				ilg.MarkLabel (loopEndLabel);
 
-				//ilg.Emit (OpCodes.Ldloc, indexLocal);
 				ilg.Emit (OpCodes.Ldloc, readerLocal);
 				ilg.Emit (OpCodes.Ldfld, posField);
 

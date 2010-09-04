0fe418b1338ef3c13d0142c371030943a169ea5e
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-09-04 12:26:42 +0200

    [daemon] Update package name for dbus-sharp-glib

--- daemon.mk
+++ daemon.mk
@@ -3,7 +3,7 @@ CSC=gmcs
 SRCS=Daemon.cs Server.cs ServerBus.cs
 
 ifdef USE_GLIB
-GLIB_FLAGS=-d:USE_GLIB -pkg:ndesk-dbus-glib-1.0 -pkg:glib-sharp-2.0
+GLIB_FLAGS=-d:USE_GLIB -pkg:dbus-sharp-glib-1.0 -pkg:glib-sharp-2.0
 endif
 
 dbus-daemon.exe: $(SRCS)

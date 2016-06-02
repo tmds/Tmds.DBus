c8f930fed71a3a4fea08553e11693753416cd819
Tom Deseyn <tom.deseyn@gmail.com>
2016-06-02 20:56:29 +0200

    samples/MediaPlayerRemote: change namespace: NetworkManager to MediaPlayerRemote

--- samples/MediaPlayerRemote/Program.cs
+++ samples/MediaPlayerRemote/Program.cs
@@ -7,7 +7,7 @@ using Tmds.DBus;
 
 #pragma warning disable 0649 // Field is never assigned to, and will always have its default value
 
-namespace NetworkManager
+namespace MediaPlayerRemote
 {
     class TrackListProperties
     {

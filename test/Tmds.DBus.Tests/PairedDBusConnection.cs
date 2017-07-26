48d3a1582298bb748ff150b1ceb7c45bdbe77696
Tom Deseyn <tom.deseyn@gmail.com>
2017-07-26 15:38:21 +0200

    Add ConnectionOptions.SynchronizationContext

--- test/Tmds.DBus.Tests/PairedDBusConnection.cs
+++ test/Tmds.DBus.Tests/PairedDBusConnection.cs
@@ -8,12 +8,10 @@ namespace Tmds.DBus.Tests
         public static async Task<Tuple<IDBusConnection, IDBusConnection>> CreateConnectedPairAsync()
         {
             var streams = PairedMessageStream.CreatePair();
-            var conn1 = new DBusConnection(streams.Item1);
-            var task1 = conn1.ConnectAsync();
-            var conn2 = new DBusConnection(streams.Item2);
-            var task2 = conn2.ConnectAsync();
-            await task1;
-            await task2;
+            var task1 = DBusConnection.CreateAndConnectAsync(streams.Item1);
+            var task2 = DBusConnection.CreateAndConnectAsync(streams.Item2);
+            var conn1 = await task1;
+            var conn2 = await task2;
             return Tuple.Create<IDBusConnection, IDBusConnection>(conn1, conn2);
         }
     }

e0cad39ca6783650e0ba868c2c18f088bb4e6c7c
Tom Deseyn <tom.deseyn@gmail.com>
2017-08-05 07:31:55 +0200

    Add PropertyChanges.GetStruct

--- src/Tmds.DBus/PropertyChanges.cs
+++ src/Tmds.DBus/PropertyChanges.cs
@@ -43,16 +43,15 @@ namespace Tmds.DBus
             => new PropertyChanges(new [] { new KeyValuePair<string, object>(prop, val) });
 
         /// <summary>
-        /// Retrieves value for a specific property. The default is returned when the property is not present.
+        /// Retrieves value for a specific property. <c>default(T)</c> is returned when the property is not present.
         /// </summary>
         /// <typeparam name="T">Type of the property.</typeparam>
         /// <param name="name">Property name.</param>
         /// <returns>
         /// Value of the property. <c>default(T)</c> when property not present.
         /// </returns>
-        public T GetValue<T>(string name)
+        public T Get<T>(string name) where T : class
         {
-            // TODO: value type?
             for (int i = 0; i < Changed.Length; i++)
             {
                 if (Changed[i].Key == name)
@@ -61,6 +60,26 @@ namespace Tmds.DBus
                 }
             }
             return default(T);
+        }
+
+        /// <summary>
+        /// Retrieves value for a specific property. <c>default(T?)</c> is returned when the property is not present.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="name">Property name.</param>
+        /// <returns>
+        /// Value of the property. <c>default(T?)</c> when property not present.
+        /// </returns>
+        public T? GetStruct<T>(string name) where T : struct
+        {
+            for (int i = 0; i < Changed.Length; i++)
+            {
+                if (Changed[i].Key == name)
+                {
+                    return (T)Changed[i].Value;
+                }
+            }
+            return default(T?);
         }
     }
 }

a29bc54faf19fa55abc4d4b8aee66654eba347f3
Tom Deseyn <tom.deseyn@gmail.com>
2017-08-05 07:40:59 +0200

    Refactor Connection ObjectDisposedException

--- src/Tmds.DBus/ConnectionOptions.cs
+++ src/Tmds.DBus/ConnectionOptions.cs
@@ -1,4 +1,4 @@
-// Copyright 2016 Tom Deseyn <tom.deseyn@gmail.com>
+// Copyright 2017 Tom Deseyn <tom.deseyn@gmail.com>
 // This software is made available under the MIT License
 // See COPYING for details
 

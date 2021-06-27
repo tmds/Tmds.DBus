5899418f8262032081df315170829ac44c54c09f
David Lechner <david@lechnology.com>
2021-06-26 21:14:14 -0500

    Add additional SignalMatchRule parameters
    This adds new properties to the SignalMatchRule to allow more complex
    matches.
    
    Issue: #57

--- src/Tmds.DBus/SignalMatchRule.cs
+++ src/Tmds.DBus/SignalMatchRule.cs
@@ -1,9 +1,12 @@
 // Copyright 2006 Alp Toker <alp@atoker.com>
 // Copyright 2016 Tom Deseyn <tom.deseyn@gmail.com>
+// Copyright 2021 David Lechner <david@lechnology.com>
 // This software is made available under the MIT License
 // See COPYING for details
 
+using System;
 using System.Text;
+using Tmds.DBus.Protocol;
 
 namespace Tmds.DBus
 {
@@ -29,6 +32,31 @@ namespace Tmds.DBus
         public ObjectPath? Path { get; set; }
 
         /// <summary>
+        /// Match messages which are sent from or to an object for which the 
+        /// object path is either the given value, or that value followed by one
+        /// or more path components. 
+        /// </summary>
+        public ObjectPath? PathNamespace { get; set; }
+
+        /// <summary>
+        /// Arg matches are special and are used for further restricting the
+        /// match based on the arguments in the body of a message.
+        /// </summary>
+        public  (int index, string arg)[] Args { get; set; }
+
+        /// <summary>
+        /// Argument path matches provide a specialized form of wildcard
+        /// matching for path-like namespaces.
+        /// </summary>
+        public (int index, string argPath)[] ArgPaths { get; set; }
+
+        /// <summary>
+        /// Match messages whose first argument is of type STRING, and is a bus
+        /// name or interface name within the specified namespace.
+        /// </summary>
+        public string Arg0Namespace { get; set; }
+
+        /// <summary>
         /// Returns the hash code for this match rule.
         /// </summary>
         public override int GetHashCode()
@@ -37,6 +65,10 @@ namespace Tmds.DBus
             hash = hash * 23 + (Interface == null ? 0 : Interface.GetHashCode());
             hash = hash * 23 + (Member == null ? 0 : Member.GetHashCode());
             hash = hash * 23 + Path.GetHashCode();
+            hash = hash * 23 + PathNamespace.GetHashCode();
+            hash = hash * 23 + (Args == null ? 0 : Args.GetHashCode());
+            hash = hash * 23 + (ArgPaths == null ? 0 : ArgPaths.GetHashCode());
+            hash = hash * 23 + (Arg0Namespace == null ? 0 : Arg0Namespace.GetHashCode());
             return hash;
         }
 
@@ -51,7 +83,34 @@ namespace Tmds.DBus
 
             return Interface == r.Interface &&
                 Member == r.Member &&
-                Path == r.Path;
+                Path == r.Path &&
+                PathNamespace == r.PathNamespace &&
+                ArgEquals(Args,r.Args) &&
+                ArgEquals(ArgPaths, r.ArgPaths) &&
+                Arg0Namespace == r.Arg0Namespace;
+        }
+
+        private static bool ArgEquals((int index, string arg)[] one, (int index, string arg)[] two)
+        {
+            if (one == two) {
+                return true;
+            }
+
+            if (one == null || two == null) {
+                return false;
+            }
+
+            if (one.Length != two.Length) {
+                return false;
+            }
+
+            for (var i = 0; i < one.Length; i++) {
+                if (one[i] != two[i]) {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -68,6 +127,10 @@ namespace Tmds.DBus
 
             Append(sb, "type", "signal");
 
+            if (sender != null)
+            {
+                Append(sb, "sender", sender);
+            }
             if (Interface != null)
             {
                 Append(sb, "interface", Interface);
@@ -80,9 +143,27 @@ namespace Tmds.DBus
             {
                 Append(sb, "path", Path.Value);
             }
-            if (sender != null)
+            if (PathNamespace != null)
             {
-                Append(sb, "sender", sender);
+                Append(sb, "path_namespace", PathNamespace.Value);
+            }
+            if (Args != null)
+            {
+                foreach (var item in Args)
+                {
+                    Append(sb, $"arg{item.index}", item.arg);
+                }
+            }
+            if (ArgPaths != null)
+            {
+                foreach (var item in ArgPaths)
+                {
+                    Append(sb, $"arg{item.index}path", item.argPath);
+                }
+            }
+            if (Arg0Namespace != null)
+            {
+                Append(sb, "arg0namespace", Arg0Namespace);
             }
 
             return sb.ToString();
--- test/Tmds.DBus.Tests/SignalMatchRuleTests.cs
+++ test/Tmds.DBus.Tests/SignalMatchRuleTests.cs
@@ -13,7 +13,47 @@ namespace Tmds.DBus.Tests
                 Path = "/org/example/test",
             };
 
-            Assert.Equal(rule.ToString(), "type='signal',interface='org.example.Test',member='TestSignal',path='/org/example/test'");
+            Assert.Equal(rule.ToString(),
+                "type='signal',interface='org.example.Test',member='TestSignal',path='/org/example/test'");
+
+            rule.Args = new[] { (2, "arg 2") };
+            rule.ArgPaths = new[] { (1, "/org/example/test/arg1") };
+            rule.Arg0Namespace = "/org/example/test/arg0";
+
+            Assert.Equal(rule.ToString(),
+                "type='signal',interface='org.example.Test',member='TestSignal',path='/org/example/test',arg2='arg 2',arg1path='/org/example/test/arg1',arg0namespace='/org/example/test/arg0'");
+        }
+
+        [Fact]
+        public void Equality()
+        {
+            var rule1 = new SignalMatchRule {
+                Interface = "org.example.Test",
+                Member = "TestSignal",
+                Path = "/org/example/test",
+                Args = new[] { (2, "arg 2") },
+                ArgPaths = new[] { (1, "/org/example/test/arg1") },
+                Arg0Namespace = "/org/example/test/arg0",
+            };
+
+            var rule2 = new SignalMatchRule {
+                Interface = "org.example.Test",
+                Member = "TestSignal",
+                Path = "/org/example/test",
+                Args = new[] { (2, "arg 2") },
+                ArgPaths = new[] { (1, "/org/example/test/arg1") },
+                Arg0Namespace = "/org/example/test/arg0",
+            };
+
+            Assert.Equal(rule1, rule2);
+
+            var rule3 = new SignalMatchRule{
+                Interface = "org.example.Test",
+                Member = "TestSignal",
+                Path = "/org/example/test",
+            };
+
+            Assert.NotEqual(rule1, rule3);
         }
     }
 }

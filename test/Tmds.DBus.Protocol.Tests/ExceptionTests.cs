a4c5f3dc193401736c016992912a112a46977369
Tom Deseyn <tom.deseyn@gmail.com>
2024-01-31 15:36:34 +0100

    Protocol: add API to control whether AddMatch emits exceptions on dispose. (#229)
    * Protocol: add API to control whether AddMatch emits exceptions on dispose.
    
    * Rename AddMatchFlags to ObserverFlags.
    
    * Rename MatchActionException to ActionException.

--- test/Tmds.DBus.Protocol.Tests/ExceptionTests.cs
+++ test/Tmds.DBus.Protocol.Tests/ExceptionTests.cs
@@ -24,8 +24,8 @@ namespace Tmds.DBus.Protocol.Tests
             disposable.Dispose();
 
             Exception ex = await tcs.Task;
-            Assert.True(MatchActionException.IsObserverDisposed(ex));
-            Assert.True(MatchActionException.IsDisposed(ex));
+            Assert.True(ActionException.IsObserverDisposed(ex));
+            Assert.True(ActionException.IsDisposed(ex));
         }
 
         [Fact]
@@ -46,8 +46,66 @@ namespace Tmds.DBus.Protocol.Tests
             conn1.Dispose();
 
             Exception ex = await tcs.Task;
-            Assert.True(MatchActionException.IsConnectionDisposed(ex));
-            Assert.True(MatchActionException.IsDisposed(ex));
+            Assert.True(ActionException.IsConnectionDisposed(ex));
+            Assert.True(ActionException.IsDisposed(ex));
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task CanOptOutObserverDisposedEmit(bool optIn)
+        {
+            var connections = PairedConnection.CreatePair();
+            using var conn1 = connections.Item1;
+            using var conn2 = connections.Item2;
+
+            Exception? exception = null;
+            var disposable = await conn1.AddMatchAsync(
+                new MatchRule(), (Message message, object? state) => "", (Exception? ex, string s, object? s1, object? s2) =>
+                {
+                    exception ??= ex;
+                }, null, null, synchronizationContext: null, optIn ? ObserverFlags.EmitOnObserverDispose : ObserverFlags.None);
+
+            disposable.Dispose();
+
+            if (optIn)
+            {
+                Assert.NotNull(exception);
+            }
+            else
+            {
+                Assert.Null(exception);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task CanOptOutConnectionDisposedEmit(bool optIn)
+        {
+            var connections = PairedConnection.CreatePair();
+            using var conn1 = connections.Item1;
+            using var conn2 = connections.Item2;
+
+            Exception? exception = null;
+            TaskCompletionSource<Exception> tcs = new();
+
+            var disposable = await conn1.AddMatchAsync(
+                new MatchRule(), (Message message, object? state) => "", (Exception? ex, string s, object? s1, object? s2) =>
+                {
+                    exception ??= ex;
+                }, null, null, synchronizationContext: null, optIn ? ObserverFlags.EmitOnConnectionDispose : ObserverFlags.None);
+
+            conn1.Dispose();
+
+            if (optIn)
+            {
+                Assert.NotNull(exception);
+            }
+            else
+            {
+                Assert.Null(exception);
+            }
         }
     }
 }

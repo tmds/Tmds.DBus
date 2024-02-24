69f1ba5629c560444c7fa1047f118f45463295c8
Tom Deseyn <tom.deseyn@gmail.com>
2024-02-24 15:51:50 +0100

    Protocol: optimize writing of numeric arrays.

--- src/Tmds.DBus.Protocol/MessageWriter.Array.cs
+++ src/Tmds.DBus.Protocol/MessageWriter.Array.cs
@@ -16,6 +16,57 @@ public ref partial struct MessageWriter
     public void WriteArray<T>(T[] value)
         where T : notnull
     {
+        if (typeof(T) == typeof(byte))
+        {
+            WriteArrayOfNumeric<byte>((byte[])(object)value);
+        }
+        else if (typeof(T) == typeof(short))
+        {
+            WriteArrayOfNumeric<short>((short[])(object)value);
+        }
+        else if (typeof(T) == typeof(ushort))
+        {
+            WriteArrayOfNumeric<ushort>((ushort[])(object)value);
+        }
+        else if (typeof(T) == typeof(int))
+        {
+            WriteArrayOfNumeric<int>((int[])(object)value);
+        }
+        else if (typeof(T) == typeof(uint))
+        {
+            WriteArrayOfNumeric<uint>((uint[])(object)value);
+        }
+        else if (typeof(T) == typeof(long))
+        {
+            WriteArrayOfNumeric<long>((long[])(object)value);
+        }
+        else if (typeof(T) == typeof(ulong))
+        {
+            WriteArrayOfNumeric<ulong>((ulong[])(object)value);
+        }
+        else if (typeof(T) == typeof(double))
+        {
+            WriteArrayOfNumeric<double>((double[])(object)value);
+        }
+        else
+        {
+            WriteArrayOfT<T>(value);
+        }
+    }
+
+    private unsafe void WriteArrayOfNumeric<T>(T[] value) where T : unmanaged
+    {
+        WriteInt32(value.Length * sizeof(T));
+        if (sizeof(T) > 4)
+        {
+            WritePadding(sizeof(T));
+        }
+        WriteRaw(MemoryMarshal.AsBytes(value.AsSpan()));
+    }
+
+    private void WriteArrayOfT<T>(T[] value)
+        where T : notnull
+    {
         ArrayStart arrayStart = WriteArrayStart(TypeModel.GetTypeAlignment<T>());
         foreach (var item in value)
         {

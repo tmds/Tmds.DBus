92b7e3eec94d44620d8f1917ee079627d776d75a
Tom Deseyn <tom.deseyn@gmail.com>
2024-03-07 21:15:00 +0100

    Protocol: Struct/Array/Dict parameter types must be valid variant types. (#257)

--- src/Tmds.DBus.Protocol/Struct.cs
+++ src/Tmds.DBus.Protocol/Struct.cs
@@ -94,8 +94,12 @@ public sealed class Struct<T1> : IDBusWritable
     public T1 Item1;
 
     public Struct(T1 item1)
-        => Item1 = item1;
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        Item1 = item1;
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -117,8 +121,13 @@ public sealed class Struct<T1,T2> : IDBusWritable
     public T2 Item2;
 
     public Struct(T1 item1, T2 item2)
-        => (Item1, Item2) = (item1, item2);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        (Item1, Item2) = (item1, item2);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -141,8 +150,14 @@ public sealed class Struct<T1,T2,T3> : IDBusWritable
     public T3 Item3;
 
     public Struct(T1 item1, T2 item2, T3 item3)
-        => (Item1, Item2, Item3) = (item1, item2, item3);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        (Item1, Item2, Item3) = (item1, item2, item3);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -167,8 +182,15 @@ public sealed class Struct<T1,T2,T3,T4> : IDBusWritable
     public T4 Item4;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4)
-        => (Item1, Item2, Item3, Item4) = (item1, item2, item3, item4);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        (Item1, Item2, Item3, Item4) = (item1, item2, item3, item4);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -195,8 +217,16 @@ public sealed class Struct<T1,T2,T3,T4,T5> : IDBusWritable
     public T5 Item5;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)
-        => (Item1, Item2, Item3, Item4, Item5) = (item1, item2, item3, item4, item5);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        (Item1, Item2, Item3, Item4, Item5) = (item1, item2, item3, item4, item5);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -225,8 +255,17 @@ public sealed class Struct<T1,T2,T3,T4,T5,T6> : IDBusWritable
     public T6 Item6;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6)
-        => (Item1, Item2, Item3, Item4, Item5, Item6) = (item1, item2, item3, item4, item5, item6);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        TypeModel.EnsureSupportedVariantType<T6>();
+        (Item1, Item2, Item3, Item4, Item5, Item6) = (item1, item2, item3, item4, item5, item6);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -257,8 +296,18 @@ public sealed class Struct<T1,T2,T3,T4,T5,T6,T7> : IDBusWritable
     public T7 Item7;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7)
-        => (Item1, Item2, Item3, Item4, Item5, Item6, Item7) = (item1, item2, item3, item4, item5, item6, item7);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        TypeModel.EnsureSupportedVariantType<T6>();
+        TypeModel.EnsureSupportedVariantType<T7>();
+        (Item1, Item2, Item3, Item4, Item5, Item6, Item7) = (item1, item2, item3, item4, item5, item6, item7);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -291,8 +340,19 @@ public sealed class Struct<T1,T2,T3,T4,T5,T6,T7,T8> : IDBusWritable
     public T8 Item8;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7, T8 item8)
-        => (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8) = (item1, item2, item3, item4, item5, item6, item7, item8);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        TypeModel.EnsureSupportedVariantType<T6>();
+        TypeModel.EnsureSupportedVariantType<T7>();
+        TypeModel.EnsureSupportedVariantType<T8>();
+        (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8) = (item1, item2, item3, item4, item5, item6, item7, item8);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -327,8 +387,20 @@ public sealed class Struct<T1,T2,T3,T4,T5,T6,T7,T8,T9> : IDBusWritable
     public T9 Item9;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7, T8 item8, T9 item9)
-        => (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9) = (item1, item2, item3, item4, item5, item6, item7, item8, item9);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        TypeModel.EnsureSupportedVariantType<T6>();
+        TypeModel.EnsureSupportedVariantType<T7>();
+        TypeModel.EnsureSupportedVariantType<T8>();
+        TypeModel.EnsureSupportedVariantType<T9>();
+        (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9) = (item1, item2, item3, item4, item5, item6, item7, item8, item9);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 
@@ -365,8 +437,21 @@ public sealed class Struct<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> : IDBusWritable
     public T10 Item10;
 
     public Struct(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7, T8 item8, T9 item9, T10 item10)
-        => (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9, Item10) = (item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
+    {
+        TypeModel.EnsureSupportedVariantType<T1>();
+        TypeModel.EnsureSupportedVariantType<T2>();
+        TypeModel.EnsureSupportedVariantType<T3>();
+        TypeModel.EnsureSupportedVariantType<T4>();
+        TypeModel.EnsureSupportedVariantType<T5>();
+        TypeModel.EnsureSupportedVariantType<T6>();
+        TypeModel.EnsureSupportedVariantType<T7>();
+        TypeModel.EnsureSupportedVariantType<T8>();
+        TypeModel.EnsureSupportedVariantType<T9>();
+        TypeModel.EnsureSupportedVariantType<T10>();
+        (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9, Item10) = (item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
+    }
 
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
         => writer.WriteStruct(ToValueTuple());
 

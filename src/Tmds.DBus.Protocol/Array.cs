e249a719815ab434d370ae6e7128c5e32bf55685
Tom Deseyn <tom.deseyn@gmail.com>
2024-03-08 11:44:13 +0100

    Protocol: review Array members.

--- src/Tmds.DBus.Protocol/Array.cs
+++ src/Tmds.DBus.Protocol/Array.cs
@@ -3,26 +3,34 @@ namespace Tmds.DBus.Protocol;
 public sealed class Array<T> : IDBusWritable
     where T : notnull
 {
-    private T[] _array;
+    private readonly T[] _array;
 
-    public T[] AsArray() => _array;
+    public Array() :
+        this(Array.Empty<T>())
+    { }
 
-    public Variant AsVariant() => Variant.FromArray(this);
-
-    public static implicit operator Variant(Array<T> value)
-        => value.AsVariant();
-
-    public Array()
-    {
-        TypeModel.EnsureSupportedVariantType<T>();
-        _array = Array.Empty<T>();
-    }
+    public Array(int length) :
+        this(new T[length])
+    { }
 
     public Array(T[] value)
     {
         TypeModel.EnsureSupportedVariantType<T>();
         _array = value ?? throw new ArgumentNullException(nameof(value));
     }
+
+    public bool IsEmpty => Length == 0;
+
+    public int Length => _array.Length;
+
+    public ref T this[int index]
+        => ref _array[index];
+
+    public Variant AsVariant()
+        => Variant.FromArray(this);
+
+    public static implicit operator Variant(Array<T> value)
+        => value.AsVariant();
 
     [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)

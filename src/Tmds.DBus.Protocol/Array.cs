4f766baa76acfcc873fdf565623535f9e3d4c5d5
Tom Deseyn <tom.deseyn@gmail.com>
2024-03-09 11:01:53 +0100

    Protocol: make Array an IList<T>. (#260)

--- src/Tmds.DBus.Protocol/Array.cs
+++ src/Tmds.DBus.Protocol/Array.cs
@@ -1,30 +1,73 @@
+using System.Collections;
+
 namespace Tmds.DBus.Protocol;
 
-public sealed class Array<T> : IDBusWritable
+public sealed class Array<T> : IDBusWritable, IList<T>, IEnumerable<T>
     where T : notnull
 {
-    private readonly T[] _array;
+    private readonly List<T> _values;
 
     public Array() :
-        this(Array.Empty<T>())
+        this(new List<T>())
     { }
 
-    public Array(int length) :
-        this(new T[length])
+    public Array(int capacity) :
+        this(new List<T>(capacity))
     { }
 
-    public Array(T[] value)
+    public Array(IEnumerable<T> collection) :
+        this(new List<T>(collection))
+    { }
+
+    private Array(List<T> values)
     {
         TypeModel.EnsureSupportedVariantType<T>();
-        _array = value ?? throw new ArgumentNullException(nameof(value));
+        _values = values;
     }
 
-    public bool IsEmpty => Length == 0;
+    public void Add(T item)
+        => _values.Add(item);
 
-    public int Length => _array.Length;
+    public void Clear()
+        => _values.Clear();
 
-    public ref T this[int index]
-        => ref _array[index];
+    public List<T>.Enumerator GetEnumerator()
+        => _values.GetEnumerator();
+
+    public int Count => _values.Count;
+
+    bool ICollection<T>.IsReadOnly
+        => false;
+
+    public T this[int index]
+    {
+        get => _values[index];
+        set => _values[index] = value;
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        => ((Array<T>)this).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable<T>)this).GetEnumerator();
+
+    public int IndexOf(T item)
+        => _values.IndexOf(item);
+
+    public void Insert(int index, T item)
+        => _values.Insert(index, item);
+
+    public void RemoveAt(int index)
+        => _values.RemoveAt(index);
+
+    public bool Contains(T item)
+        => _values.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+        => _values.CopyTo(array, arrayIndex);
+
+    public bool Remove(T item)
+        => _values.Remove(item);
 
     public Variant AsVariant()
         => Variant.FromArray(this);
@@ -34,5 +77,12 @@ public sealed class Array<T> : IDBusWritable
 
     [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026")] // this is a supported variant type.
     void IDBusWritable.WriteTo(ref MessageWriter writer)
-        => writer.WriteArray<T>(_array);
+    {
+#if NET5_0_OR_GREATER
+        Span<T> span = CollectionsMarshal.AsSpan(_values);
+        writer.WriteArray<T>(span);
+#else
+        writer.WriteArray(_values);
+#endif
+    }
 }

033992cbf4d979db37e7663b61fee06baec8c401
Tom Deseyn <tom.deseyn@gmail.com>
2024-06-11 22:57:48 +0200

    Protocol: SafeHandle types must have a parameterless constructor. (#289)

--- src/Tmds.DBus.Protocol/Reader.Array.cs
+++ src/Tmds.DBus.Protocol/Reader.Array.cs
@@ -41,7 +41,7 @@ public ref partial struct Reader
     public VariantValue[] ReadArrayOfVariantValue()
         => ReadArrayOfT<VariantValue>();
 
-    public T[] ReadArrayOfHandle<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]T>() where T : SafeHandle
+    public T[] ReadArrayOfHandle<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]T>() where T : SafeHandle, new()
         => ReadArrayOfT<T>();
 
     [RequiresUnreferencedCode(Strings.UseNonGenericReadArray)]

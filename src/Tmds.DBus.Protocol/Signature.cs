233a208cf3be35b3b962f7da67a9614e620a112c
Tom Deseyn <tom.deseyn@gmail.com>
2024-11-21 07:51:38 +0100

    Make static Signature signature properties internal.

--- src/Tmds.DBus.Protocol/Signature.cs
+++ src/Tmds.DBus.Protocol/Signature.cs
@@ -5,20 +5,20 @@ public readonly struct Signature
     private readonly byte[]? _value;
 
     // note: C# compiler treats these as static data.
-    public static ReadOnlySpan<byte> Byte => new byte[] { (byte)'y' };
-    public static ReadOnlySpan<byte> Boolean => new byte[] { (byte)'b' };
-    public static ReadOnlySpan<byte> Int16 => new byte[] { (byte)'n' };
-    public static ReadOnlySpan<byte> UInt16 => new byte[] { (byte)'q' };
-    public static ReadOnlySpan<byte> Int32 => new byte[] { (byte)'i' };
-    public static ReadOnlySpan<byte> UInt32 => new byte[] { (byte)'u' };
-    public static ReadOnlySpan<byte> Int64 => new byte[] { (byte)'x' };
-    public static ReadOnlySpan<byte> UInt64 => new byte[] { (byte)'t' };
-    public static ReadOnlySpan<byte> Double => new byte[] { (byte)'d' };
-    public static ReadOnlySpan<byte> UnixFd => new byte[] { (byte)'h' };
-    public static ReadOnlySpan<byte> String => new byte[] { (byte)'s' };
-    public static ReadOnlySpan<byte> ObjectPath => new byte[] { (byte)'o' };
-    public static ReadOnlySpan<byte> Sig => new byte[] { (byte)'g' }; // Name can not be the same as enclosing type.
-    public static ReadOnlySpan<byte> Variant => new byte[] { (byte)'v' };
+    internal static ReadOnlySpan<byte> Byte => new byte[] { (byte)'y' };
+    internal static ReadOnlySpan<byte> Boolean => new byte[] { (byte)'b' };
+    internal static ReadOnlySpan<byte> Int16 => new byte[] { (byte)'n' };
+    internal static ReadOnlySpan<byte> UInt16 => new byte[] { (byte)'q' };
+    internal static ReadOnlySpan<byte> Int32 => new byte[] { (byte)'i' };
+    internal static ReadOnlySpan<byte> UInt32 => new byte[] { (byte)'u' };
+    internal static ReadOnlySpan<byte> Int64 => new byte[] { (byte)'x' };
+    internal static ReadOnlySpan<byte> UInt64 => new byte[] { (byte)'t' };
+    internal static ReadOnlySpan<byte> Double => new byte[] { (byte)'d' };
+    internal static ReadOnlySpan<byte> UnixFd => new byte[] { (byte)'h' };
+    internal static ReadOnlySpan<byte> String => new byte[] { (byte)'s' };
+    internal static ReadOnlySpan<byte> ObjectPath => new byte[] { (byte)'o' };
+    internal static ReadOnlySpan<byte> Sig => new byte[] { (byte)'g' }; // Name can not be the same as enclosing type.
+    internal static ReadOnlySpan<byte> Variant => new byte[] { (byte)'v' };
 
     internal byte[] Data => _value ?? Array.Empty<byte>();
 

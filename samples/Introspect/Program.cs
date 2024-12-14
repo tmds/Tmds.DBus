b774bb09b774eb33982703b10517c8843a357950
Thomas Ibel <tibel@users.noreply.github.com>
2024-12-14 10:40:35 +0100

    Revert "Introspect: move to protocol api"
    This reverts commit 607d1edf75e66350ec17b6c9dc6629acb5f2d488.

--- samples/Introspect/Program.cs
+++ samples/Introspect/Program.cs
@@ -1,29 +1,54 @@
 using System;
-using Tmds.DBus.Protocol;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Tmds.DBus;
 
-if (args.Length != 3)
+namespace Introspect
 {
-    Console.WriteLine("Usage: --session/--system <servicename> <objectpath>");
-    return -1;
+    [DBusInterface("org.freedesktop.DBus.Introspectable")]
+    public interface IIntrospectable : IDBusObject
+    {
+        Task<string> IntrospectAsync();
+    }
+
+    public class Program
+    {
+        private static async Task InspectAsync(bool sessionNotSystem, string serviceName, string objectPath)
+        {
+            using (var connection = new Connection(sessionNotSystem ? Address.Session : Address.System))
+            {
+                connection.StateChanged += (s, e) => OnStateChanged(e);
+                await connection.ConnectAsync();
+                var introspectable = connection.CreateProxy<IIntrospectable>(serviceName, objectPath);
+                var xml = await introspectable.IntrospectAsync();
+                Console.WriteLine(xml);
+            }
+        }
+
+        public static void OnStateChanged(ConnectionStateChangedEventArgs e)
+        {
+            if (e.State == ConnectionState.Disconnected && e.DisconnectReason != null)
+            {
+                Console.WriteLine($"Connection closed: {e.DisconnectReason.Message}");
+            }
+        }
+
+        public static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: --session/--system <servicename> <objectpath>");
+                return -1;
+            }
+            bool sessionNotSystem = args[0] != "--system";
+            var service = args[1];
+            var objectPath = args[2];
+
+            Task.Run(() => InspectAsync(sessionNotSystem, service, objectPath)).Wait();
+
+            return 0;
+        }
+    }
 }
-bool sessionNotSystem = args[0] != "--system";
-var service = args[1];
-var objectPath = args[2];
-
-using var connection = new Connection(sessionNotSystem ? Address.Session! : Address.System!);
-await connection.ConnectAsync();
-
-var xml = await connection.CallMethodAsync(CreateMessage(objectPath), (Message m, object? s) => m.GetBodyReader().ReadString(), null);
-MessageBuffer CreateMessage(string path)
-{
-    using var writer = connection.GetMessageWriter();
-    writer.WriteMethodCallHeader(
-        destination: service,
-        path: path,
-        @interface: "org.freedesktop.DBus.Introspectable",
-        member: "Introspect");
-    return writer.CreateMessage();
-}
-
-Console.WriteLine(xml);
-return 0;
